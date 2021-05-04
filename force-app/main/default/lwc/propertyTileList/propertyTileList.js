cf10f904166f5873b8aa7b7ffbc9beb9e5dfe513
Max Euler <maxjuliuseuler@gmail.com>
2021-05-04 09:15:42 +0200

    wire properties in propertyTileList lwc

--- force-app/main/default/lwc/propertyTileList/propertyTileList.js
+++ force-app/main/default/lwc/propertyTileList/propertyTileList.js
@@ -1,3 +1,24 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import getPagedPropertyList from '@salesforce/apex/PropertyController.getPagedPropertyList';
 
-export default class PropertyTileList extends LightningElement {}
+const PAGE_SIZE = 9;
+
+export default class PropertyTileList extends LightningElement {
+    pageNumber = 1;
+    pageSize = PAGE_SIZE;
+
+    searchKey = '';
+    maxPrice = 9999999;
+    minBedrooms = 0;
+    minBathrooms = 0;
+
+    @wire(getPagedPropertyList, {
+        searchKey: '$searchKey',
+        maxPrice: '$maxPrice',
+        minBedrooms: '$minBedrooms',
+        minBathrooms: '$minBathrooms',
+        pageSize: '$pageSize',
+        pageNumber: '$pageNumber'
+    })
+    properties;
+}
