4eb800090a2bf7507b6ff086cd613d70b15955d4
Max Euler <maxjuliuseuler@gmail.com>
2021-05-06 11:21:53 +0200

    fetch pictures in propertyCarousel lwc

--- force-app/main/default/lwc/propertyCarousel/propertyCarousel.js
+++ force-app/main/default/lwc/propertyCarousel/propertyCarousel.js
@@ -1,5 +1,7 @@
 import { LightningElement, wire, api } from 'lwc';
 import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+import { refreshApex } from '@salesforce/apex';
+import getPictures from '@salesforce/apex/PropertyController.getPictures';
 
 import ADDRESS_FIELD from '@salesforce/schema/Property__c.Address__c';
 import CITY_FIELD from '@salesforce/schema/Property__c.City__c';
@@ -9,9 +11,26 @@ const FIELDS = [ADDRESS_FIELD, CITY_FIELD, DESCRIPTION_FIELD];
 
 export default class PropertyCarousel extends LightningElement {
     @api recordId;
+    urls;
+    pictures;
 
     @wire(getRecord, { recordId: '$recordId', fields: FIELDS })
     property
+
+    @wire(getPictures, { propertyId: '$recordId'})
+    wiredPictures(pictures) {
+        this.pictures = pictures;
+        if (pictures.data) {
+            const files = pictures.data;
+            if (Array.isArray(files) && files.length) {
+                this.urls = files.map(file => 
+                    '/sfc/servlet.shepherd/version/download/' + file.Id
+                );
+            } else {
+                this.urls = null;
+            }
+        }
+    }
 
     get address() {
         return getFieldValue(this.property.data, ADDRESS_FIELD);
@@ -20,4 +39,9 @@ export default class PropertyCarousel extends LightningElement {
     get description() {
         return getFieldValue(this.property.data, DESCRIPTION_FIELD);
     }
+
+    handleUploadFinished() {
+        refreshApex(this.pictures);
+    }
+
 }
